5c6236b4ac74c4ad7030e0a3ebd5ebeb5020f95f
Jonathan Ong <jonathanrichardong@gmail.com>
2014-09-16 00:00:52 -0700

    add coverage

--- test/test.js
+++ test/test.js
@@ -1,11 +1,19 @@
 var request = require('supertest')
 var koa = require('koa')
 
-var qs = require('./')
+var qs = require('..')
 
 describe('Koa Querystring', function () {
   it('should work', function (done) {
     var app = qs(koa())
+
+    app.use(function* (next) {
+      try {
+        yield* next
+      } catch (err) {
+        console.error(err.stack)
+      }
+    })
 
     app.use(function* () {
       this.query.should.eql({
@@ -17,7 +25,6 @@ describe('Koa Querystring', function () {
       this.query.should.eql({
         a: [1, 2]
       })
-      this.querystring.should.equal('a[0]=1&a[1]=2')
       this.querystring = 'a[0]=1&a[1]=2&a[2]=3'
       this.query.should.eql({
         a: [1, 2, 3]
